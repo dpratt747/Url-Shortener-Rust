624907735d72870dcdac834b37b600042b0cc9b5
David Pratt <dpratt747@gmail.com>
2025-05-16 22:02:18 +0100

    working shared db between service/endpoint and schedular

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use crate::services::url_shortener_service::UrlShortenerService;
 use actix_web::{web, App, HttpServer};
 use persistence::database::InMemoryDatabase;
 use std::collections::HashMap;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
@@ -23,7 +23,7 @@ async fn start_scheduler(db: Box<dyn DatabaseAlg>) -> Result<(), Box<dyn std::er
     let every_ten_seconds = "1/10 * * * * *";
     let every_second = "1/1 * * * * *";
 
-    let all_stored_map: HashMap<LongUrl, ShortUrl> = db.get_all();
+    // let all_stored_map: HashMap<LongUrl, ShortUrl> = db.get_all();
         // .iter()
         // .map(|(long_url, (short_url, _))| (long_url.0.clone(), short_url.0.clone()))
         // .collect();
@@ -31,10 +31,11 @@ async fn start_scheduler(db: Box<dyn DatabaseAlg>) -> Result<(), Box<dyn std::er
     // Add a job that runs every 10 seconds
     sched.add(
         Job::new_async(every_second, move |_uuid, _l| {
-            let map = all_stored_map.clone();
+            let get_all_stored = db.get_all();
+            // let new_db = db.get_all();
             Box::pin(async move {
                 println!("Scheduled job running at {:?}", Utc::now());
-                println!("get_all: {:?}", map);
+                println!("all stored urls: {:?}", get_all_stored);
                 // Ok(())
             })
         })?
@@ -46,7 +47,9 @@ async fn start_scheduler(db: Box<dyn DatabaseAlg>) -> Result<(), Box<dyn std::er
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let db = InMemoryDatabase::new(HashMap::new());
+    let db = Arc::new(Mutex::new(InMemoryDatabase::new(HashMap::new())));
+    // db.store(LongUrl("some long url".to_string()), ShortUrl("Some short url".to_string()));
+    
     let service = UrlShortenerService::new(Box::new(db.clone()));
     let service_data = web::Data::new(Mutex::new(service));
 
--- src/persistence/database.rs
+++ src/persistence/database.rs
@@ -1,6 +1,7 @@
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 // use utoipa::openapi::KnownFormat::Duration;
 use utoipa::PartialSchema;
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize, utoipa::ToSchema)]
@@ -10,9 +11,23 @@ pub(crate) struct ShortUrl(pub(crate) String);
 pub(crate) struct LongUrl(pub(crate) String);
 
 pub trait DatabaseAlg: Send + Sync {
-    fn store(&mut self, long_url: LongUrl, short_url: ShortUrl) -> ();
+    fn store(&mut self, long_url: LongUrl, short_url: ShortUrl);
     fn get_all(&self) -> HashMap<LongUrl, ShortUrl>;
     fn get_long_url_with_short_url(&self, short_url: ShortUrl) -> Option<LongUrl>;
+}
+
+impl DatabaseAlg for Arc<Mutex<InMemoryDatabase>> {
+    fn store(&mut self, long_url: LongUrl, short_url: ShortUrl) {
+        self.lock().unwrap().store(long_url, short_url);
+    }
+
+    fn get_all(&self) -> HashMap<LongUrl, ShortUrl> {
+        self.lock().unwrap().get_all()
+    }
+
+    fn get_long_url_with_short_url(&self, short_url: ShortUrl) -> Option<LongUrl> {
+        self.lock().unwrap().get_long_url_with_short_url(short_url)
+    }
 }
 
 #[derive(Clone)]
